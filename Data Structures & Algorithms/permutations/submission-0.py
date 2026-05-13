91317529738ad3db48f009fa7482e758d0755814
Kevin Yin <151800524+Elliptic461@users.noreply.github.com>
2026-05-13 16:20:27 -0400

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 0:
+            return [[]]
+        
+        # creates a subarray starting at index one
+        perms = self.permute(nums[1:])
+        result = []
+
+
+        for p in perms:
+            for i in range(len(p) + 1):
+                p_copy = p.copy()
+                p_copy.insert(i, nums[0])
+                result.append(p_copy)
+
+        return result
+
+
+        
