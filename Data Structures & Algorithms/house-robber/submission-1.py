cf79d228b71b7f8ca375c5b0e622d3a6dc619fae
Kevin Yin <151800524+Elliptic461@users.noreply.github.com>
2026-06-28 13:42:33 -0400

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+
+        rob1, rob2 = 0, 0 # dp[i - 2], dp[i - 1]
+
+        # [rob1, rob2, n, n+1, ...]
+        for n in nums:
+            curr = max(n + rob1, rob2)
+            rob1 = rob2 
+            rob2 = curr # updated to be "n" 
+        return rob2
+
+
+        
