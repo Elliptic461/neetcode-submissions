f892dbb374adeb376d98900586693443a7fdfc7c
Kevin Yin <151800524+Elliptic461@users.noreply.github.com>
2026-07-01 15:54:42 -0400

    Add: coin-change - submission-1

---
+++ Data Structures & Algorithms/coin-change/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    # Runtime: O(amount * len(coins))  = O(n*t)
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        # Size amount + 1, that is filled with amount + 1 value (placeholder)
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0 # Base case: it takes 0 coin to make the amount 0
+
+        for a in range(1, amount + 1):
+            for c in coins:
+                # If amount - coin does not go negative
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1 + dp[a - c])
+        
+        return dp[amount] if dp[amount] != amount + 1 else -1
